8135116d30c4e42844e2874185ede0b7311dd2fe
Alvin J. Choi <31370892+alvinjchoi@users.noreply.github.com>
2025-11-02 10:26:07 -0800

    chore: remove redundant root docs

--- check_parent_flow_id.sql
+++
@@ -1,5 +0,0 @@
--- Check if parent_flow_id column exists in flows table
-SELECT column_name, data_type, is_nullable
-FROM information_schema.columns
-WHERE table_name = 'flows'
-ORDER BY ordinal_position;
